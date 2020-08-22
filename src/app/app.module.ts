9190ef5f492c13960ea2957061f2244ac82e5ec5
Ihor Holovko <ihorholovko7@gmail.com>
2020-08-22 23:45:17 +0300

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { FrontPageComponent } from './pages/front-page/front-page.component';
+import { TaskPageComponent } from './pages/task-page/task-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, TasksListComponent, AdminPageComponent, FrontPageComponent, TaskPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
