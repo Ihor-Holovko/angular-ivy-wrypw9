4a890f6816cb18776846b5d226fd19370598e62f
Ivan <chomatools@gmail.com>
2020-08-23 19:44:52 +0300

    --

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { AppComponent } from './app.component';
+import { TaskPageComponent } from './components/task-page/task-page.component';
 
 
+
+// const routes: Routes = []; // sets up routes constant where you define your routes
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: TaskPageComponent
+  },
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
-import { FrontPageComponent } from './pages/front-page/front-page.component';
-import { TaskPageComponent } from './pages/task-page/task-page.component';
+import { TaskPageComponent } from './components/task-page/task-page.component';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, TasksListComponent, AdminPageComponent, FrontPageComponent, TaskPageComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [ AppComponent, HelloComponent, TasksListComponent, TaskPageComponent ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
